320458ceee4855df6e2af48757eb339c56fb2a33
勇 黄 <conan198581@outlook.com>
2019-03-01 17:33:00 +0800

    201903011732

---
+++ ZSZ.FrontWeb/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZSZ.IService;
+
+namespace ZSZ.FrontWeb.Controllers
+{
+    public class MainController : Controller
+    {
+        public ICityService CityService { get; set; }
+        // GET: Main
+        public ActionResult Index()
+        {
+            var i = CityService.AddNew("北京");
+            return Content(i.ToString());
+        }
+    }
+}
---
+++ ZSZ.Service/CityService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZSZ.IService;
+using ZSZ.Service.Data;
+using ZSZ.Service.Entities;
+
+namespace ZSZ.Service
+{
+    public class CityService : ICityService
+    {
+        public long AddNew(string cityName)
+        {
+            using (MyDbContext ctx = new MyDbContext())
+            {
+                BaseService<CityEntity> baseService = new BaseService<CityEntity>(ctx);
+                var cityItem = baseService.GetAll().Any(x => x.Name == cityName);
+                if (cityName == null)
+                {
+                    throw new Exception("该城市已经存在");
+                }
+                CityEntity cityEntity = new CityEntity();
+                cityEntity.Name = cityName;
+                ctx.CityEntities.Add(cityEntity);
+                ctx.SaveChanges();
+                return cityEntity.Id;
+            }
+        }
+    }
+}
