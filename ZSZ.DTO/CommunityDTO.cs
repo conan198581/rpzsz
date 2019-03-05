932a300eab63419889c27d8c1d4e65744d20923b
勇 黄 <conan198581@outlook.com>
2019-03-05 17:07:34 +0800

    2019年3月5日 17点07分 绿水青山

---
+++ ZSZ.DTO/CommunityDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZSZ.DTO
+{
+    public class CommunityDTO : BaseDTO
+    {
+        public String Name { get; set; }
+        public long RegionId { get; set; }
+        public String Location { get; set; }
+        public String Traffic { get; set; }
+        public int? BuiltYear { get; set; }
+    }
+}
